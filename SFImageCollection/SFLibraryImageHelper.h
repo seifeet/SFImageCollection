e6e66c2c6c6771b02d4b8e2a61dc6ff022e070ac
AT <at@seifeet.com>
2014-04-27 21:04:53 -0700

    load images on demand

---
+++ SFImageCollection/SFLibraryImageHelper.h
@@ -0,0 +1,13 @@
+//
+//  SFLibraryImageHelper.h
+//  SFImageCollection
+//
+//  Created by Andrey Tabachnik on 4/25/14.
+//  Copyright (c) 2014 SF. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SFLibraryImageHelper : NSObject
+
+@end
---
+++ SFImageCollection/SFLibraryImageHelper.m
@@ -0,0 +1,13 @@
+//
+//  SFLibraryImageHelper.m
+//  SFImageCollection
+//
+//  Created by Andrey Tabachnik on 4/25/14.
+//  Copyright (c) 2014 SF. All rights reserved.
+//
+
+#import "SFLibraryImageHelper.h"
+
+@implementation SFLibraryImageHelper
+
+@end
